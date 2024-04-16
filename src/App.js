c45af63be986dabf61353abf2212bef82138c205
rohitbarhe0002 <rohitbarche99@gmail.com>
2024-04-16 13:03:47 +0530

    cart-with-rtk

--- src/App.js
+++ src/App.js
@@ -1,80 +1,133 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import "./App.css";
+// const initialItems = [
+//   {
+//     id: 1,
+//     name: "Adidas",
+//     itemDesc: "EQT Gozezila",
+//     itemImg:
+//       "https://www.harley-davidson.com/content/dam/h-d/images/product-images/parts/batch-4/93300103/93300103_TT.jpg?impolicy=myresize&rw=700",
+//     price: 280,
+//     quantity: 1,
+//     totalPrice: 280,
+//   },
+//   {
+//     id: 2,
+//     name: "Adidas",
+//     itemDesc: "EQT Gozezila",
+//     itemImg:
+//       "https://www.harley-davidson.com/content/dam/h-d/images/product-images/parts/batch-4/93300103/93300103_TT.jpg?impolicy=myresize&rw=700",
+//     price: 280,
+//     quantity: 1,
+//     totalPrice: 280,
+//   },
+//   {
+//     id: 3,
+//     name: "Adidas",
+//     itemDesc: "EQT Gozezila",
+//     itemImg:
+//       "https://www.harley-davidson.com/content/dam/h-d/images/product-images/parts/batch-4/93300103/93300103_TT.jpg?impolicy=myresize&rw=700",
+//     price: 280,
+//     quantity: 1,
+//     totalPrice: 280,
+//   },
 
-const initialItems = [
-  { id: 1, name: "Item 1", price: 10, quantity: 1 },
-  { id: 2, name: "Item 2", price: 15, quantity: 1 },
-  { id: 3, name: "Item 3", price: 20, quantity: 1 },
-];
+//   {
+//     id: 4,
+//     name: "Adidas",
+//     itemDesc: "EQT Gozezila",
+//     itemImg:
+//       "https://www.harley-davidson.com/content/dam/h-d/images/product-images/parts/batch-4/93300103/93300103_TT.jpg?impolicy=myresize&rw=700",
+//     price: 280,
+//     quantity: 1,
+//     totalPrice: 280,
+//   },
+// ];
 
-const App = () => {
-  const [cart, setCart] = useState([]);
+// const App = () => {
+//   const [cart, setCart] = useState([]);
 
-  const addToCart = (item) => {
-    // Increment the quantity in initialItems array
-    const itemIsAdded = cart.findIndex((itm) => itm.id === item.id);
-    if (itemIsAdded !== -1) {
-      alert("Item already added");
-      return;
-    }
+//   const addToCart = (item) => {
+//     const itemIsAdded = cart.findIndex((itm) => itm.id === item.id);
+//     if (itemIsAdded !== -1) {
+//       alert("Item already added");
+//       return;
+//     }
+//     setCart([...cart, { ...item }]);
+//   };
 
-    setCart([...cart, { ...item, totalPrice: item.price }]);
-  };
+//   const removeFromCart = (itemId) => {
+//     setCart(cart.filter((item) => item.id !== itemId));
+//   };
 
-  const removeFromCart = (itemId) => {
-    setCart(cart.filter((item) => item.id !== itemId));
-  };
+//   const updateQuantity = (itemId, action) => {
+//     const updatedCart = cart.map((item) => {
+//       if (item.id === itemId) {
+//         let newQuantity;
+//         if (action === "INCR") {
+//           newQuantity = item.quantity + 1;
+//         } else {
+//           newQuantity = item.quantity - 1 >= 0 ? item.quantity - 1 : 0;
+//         }
+//         return {
+//           ...item,
+//           quantity: newQuantity,
+//           totalPrice: newQuantity * item.price,
+//         };
+//       }
+//       return item;
+//     });
+//     setCart(updatedCart);
+//   };
 
-  const updateQuantity = (itemId, action) => {
-    const updatedCart = cart.map((item) => {
-      if (item.id === itemId) {
-        let newQuantity;
-        if (action === "INCR") {
-          newQuantity = item.quantity + 1;
-        } else {
-          newQuantity = item.quantity - 1 >= 0 ? item.quantity - 1 : 0; // Ensure quantity doesn't go below zero
-        }
-        return {
-          ...item,
-          quantity: newQuantity,
-          totalPrice: newQuantity * item.price,
-        };
-      }
-      return item;
-    });
-    setCart(updatedCart);
-  };
+//   return (
+//     <div className="cards-container">
+//       <ul>
+//         {initialItems.map((item) => (
+//           <li key={item.id}>
+//             {item.name} - ${item.price}
+//             <button onClick={() => addToCart(item)}>Add to Cart</button>
+//           </li>
+//         ))}
+//       </ul>
+//       <div>
+//         <h2>Cart</h2>
+//         <ul>
+//           {cart.map((item) => (
+//             <li key={item.id}>
+//               {item.name} - ${item.price} - Quantity:
+//               <button onClick={() => updateQuantity(item.id, "INCR")}>+</button>
+//               {item.quantity}
+//               <button onClick={() => updateQuantity(item.id, "DECR")}>-</button>
+//               <button onClick={() => removeFromCart(item.id)}>Remove</button>
+//               <p>Total Price: ${item.totalPrice}</p>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
 
+// export default App;
+
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CartList from "./components/CartList";
+import ProductList from "./components/ProductList";
+function App() {
   return (
-    <div>
-      <h1>Simple React Shopping Cart</h1>
+    <BrowserRouter>
       <div>
-        <h2>Items</h2>
-        <ul>
-          {initialItems.map((item) => (
-            <li key={item.id}>
-              {item.name} - ${item.price}
-              <button onClick={() => addToCart(item)}>Add to Cart</button>
-            </li>
-          ))}
-        </ul>
+        {/* <Home /> */}
+        {/* <Post /> */}
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/cart" element={<CartList />} />
+        </Routes>
       </div>
-      <div>
-        <h2>Cart</h2>
-        <ul>
-          {cart.map((item) => (
-            <li key={item.id}>
-              {item.name} - ${item.price} - Quantity:
-              <button onClick={() => updateQuantity(item.id, "INCR")}>+</button>
-              {item.quantity}
-              <button onClick={() => updateQuantity(item.id, "DECR")}>-</button>
-              <button onClick={() => removeFromCart(item.id)}>Remove</button>
-              <p>Total Price: ${item.totalPrice}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
